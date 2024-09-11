f7d2a5748b8c8d1a75c9e383d3939315694ea8d3
SharomProject <proyectosharom@gmail.com>
2024-09-10 19:55:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const recuperarDatos = () => {
     method: 'POST',
 	headers: {
 		'Content-Type':'application/json'
-	}
+	},
     body: JSON.stringify(formData)
   })
   .then(response => response.text())
