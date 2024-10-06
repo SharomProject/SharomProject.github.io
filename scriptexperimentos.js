250aff17b07cd34e9ed3e00a859305ed05e65efc
SharomProject <proyectosharom@gmail.com>
2024-10-06 11:56:09 -0500

    Update scriptexperimentos.js

--- scriptexperimentos.js
+++ scriptexperimentos.js
@@ -1,34 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tipoCuestionario = () => {
-        const idexperimento = localStorage.getItem('exp'); // Obtiene el valor de localStorage
-        var mensajeExp = "";
-
-        // Switch para determinar el mensaje a mostrar
-        switch (idexperimento) {
-            case 'BC':
-                mensajeExp = "Situación para beneficio compartido";
-                break;
-            case 'PC':
-                mensajeExp = "Situación para publicar la corrupción";
-                break;
-            case 'RC':
-                mensajeExp = "Situación para recordatorio comportamental";
-                break;
-            case 'SC':
-                mensajeExp = "Situación para sin control";
-                break;
-            default:
-                mensajeExp = "XD";
-        }
-
-        // Inserta el mensaje en el div con id "mensaje"
-        document.getElementById('mensaje').textContent = mensajeExp;
-    };
-
     // Llama a la función tipoCuestionario cuando el DOM esté listo
     tipoCuestionario();
 });
 
+const tipoCuestionario = () => {
+    const idexperimento = localStorage.getItem("exp"); // Obtiene el valor de localStorage
+    var mensajeExp = "";
+
+    // Switch para determinar el mensaje a mostrar
+    switch (idexperimento.charAt(0)) {
+        case 'B':
+            mensajeExp = "Situación para beneficio compartido";
+            break;
+        case 'P':
+            mensajeExp = "Situación para publicar la corrupción";
+            break;
+        case 'R':
+            mensajeExp = "Situación para recordatorio comportamental";
+            break;
+        case 'S':
+            mensajeExp = "Situación para sin control";
+            break;
+        default:
+            mensajeExp = "XD";
+    }
+
+    // Inserta el mensaje en el div con id "mensaje"
+    document.getElementById('mensaje').textContent = mensajeExp;
+};
 const enviarrespuestas = () => {
     const respuestas = {
         idexperimento: localStorage.getItem('exp'),
@@ -44,4 +43,17 @@ const enviarrespuestas = () => {
         r9: 'si',
         r10: 'si'
     }
+
+    //enviar respuestas
+    //alert ok
+    alert("oka");
+    //alert nose
 }
+
+const formulario = document.getElementById("form");
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    enviarrespuestas();
+    location.replace("https://localhost:7063/agracemiento.html");
+});
